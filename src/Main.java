44f83475317c5b3cafbdef01bc0ad0c09d6f8506
bia nagae <138304130+beatriz-nagae@users.noreply.github.com>
2025-09-26 02:27:42 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,26 +7,24 @@ public class Main {
     public static void main(String[] args) {
 
        Integer tentativas = 0;
-       Integer numeroCerto;
-       Integer numeroDado;
+       Integer numeroCerto = 0;
+       Integer numeroDado = 0;
 
-
+       Random random = new Random();
       Scanner scanner = new Scanner(System.in);
         System.out.println("Olá! Seja bem vindo ao jogo da Adivinhação");
         System.out.println("Na sorte, você se garante?");
         System.out.println("Agora, digite um numero de 1 a 100");
-        scanner.nextInt(numeroDado);
 
-        Do while(numeroDado == numeroCerto || tentativas <= 5){
-            int numeroCerto = new Random().nextInt(100);
-
+       while(numeroDado.equals(numeroCerto) || tentativas <= 5)
+             numeroCerto = random.nextInt(100);
 
             if(numeroDado == numeroCerto){
                 System.out.println("Arrasou! Acertou mesmo!");
             }
             else {
                 System.out.println("tente de novo :c");
+                 tentativas = tentativas + 1;
             }
         }
     }
-}
