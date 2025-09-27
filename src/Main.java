d55498c7fde836e9488672f29834e51383a68dfc
beatriz-nagae <bh.prestashop@gmail.com>
2025-09-26 22:21:58 -0300

    HOTFIX code working now

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,39 @@
 import java.util.Random;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int tentativas = 0;
+        final int maxTentativas = 5;
 
-       Integer tentativas = 0;
-       Integer numeroCerto;
-       Integer numeroDado;
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
 
+        int numeroCerto = random.nextInt(100) + 1; // número entre 1 e 100
 
-      Scanner scanner = new Scanner(System.in);
-        System.out.println("Olá! Seja bem vindo ao jogo da Adivinhação");
+        System.out.println("Olá! Seja bem-vindo ao jogo da Adivinhação!");
         System.out.println("Na sorte, você se garante?");
-        System.out.println("Agora, digite um numero de 1 a 100");
-        scanner.nextInt(numeroDado);
+        System.out.println("Tente adivinhar o número de 1 a 100.");
 
-        Do while(numeroDado == numeroCerto || tentativas <= 5){
-            int numeroCerto = new Random().nextInt(100);
+        boolean acertou = false;
 
+        while (tentativas < maxTentativas && !acertou) {
+            System.out.print("Digite seu palpite: ");
+            int numeroDado = scanner.nextInt();
 
-            if(numeroDado == numeroCerto){
+            if (numeroDado == numeroCerto) {
                 System.out.println("Arrasou! Acertou mesmo!");
-            }
-            else {
-                System.out.println("tente de novo :c");
+                acertou = true;
+            } else {
+                System.out.println("Tente de novo :c");
+                tentativas++;
             }
         }
+
+        if (!acertou) {
+            System.out.println("Suas tentativas acabaram! O número era: " + numeroCerto);
+        }
+
+        scanner.close();
     }
 }
